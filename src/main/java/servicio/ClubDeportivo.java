0b6722e47aeebb143ae720b66348cb61c741b80a
alvarolrx <alvarollorenteruiz@gmail.com>
2026-02-14 14:30:53 +0100

    Implementación altaSocio en la clase de servicio ClubDeportivo

--- src/main/java/servicio/ClubDeportivo.java
+++ src/main/java/servicio/ClubDeportivo.java
@@ -3,6 +3,7 @@ package servicio;
 import Entidades.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class ClubDeportivo {
@@ -21,6 +22,24 @@ public class ClubDeportivo {
         if (emf != null && emf.isOpen()) {
             emf.close();
         }
+    }
+
+    public void altaSocio(Socio socio) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(socio);
+            tx.commit();
+            em.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
     }
 
 }
